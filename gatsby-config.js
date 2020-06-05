3883b4bf9f892f3d6aad014d8b01e044d649cd3d
Makoto Watanabe <mktwtnb1989@gmail.com>
2020-06-05 22:42:46 +0100

    Introduced katex plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -102,6 +102,13 @@ module.exports = {
           },
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
+          {
+            resolve: "gatsby-remark-katex",
+            options: {
+              // Add any KaTeX options from https://github.com/KaTeX/KaTeX/blob/master/docs/options.md here
+              strict: `ignore`
+            },
+          },
         ],
       },
     },
