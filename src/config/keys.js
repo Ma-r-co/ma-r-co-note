a0ab8d37aa61e5112e9db4f793a9f912d4340783
Makoto Watanabe <mktwtnb1989@gmail.com>
2020-06-09 23:29:57 +0100

    test siteRecaptchaKey

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,5 +1,5 @@
-var siteRecaptchaKey = process.env.SITE_RECAPTCHA_KEY;
-if (typeof process.env.SITE_RECAPTCHA_KEY === 'undefined') {
+var siteRecaptchaKey = process.env.GATSBY_APP_SITE_RECAPTCHA_KEY;
+if (typeof siteRecaptchaKey === 'undefined') {
   const keys = require('./dev');
   siteRecaptchaKey = keys.siteRecaptchaKey;
 }
