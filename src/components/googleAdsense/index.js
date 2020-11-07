98c62a99022881ae140b5241c53fecc033a548b8
Makoto Watanabe <mktwtnb1989@gmail.com>
2020-11-07 18:09:35 +0000

    2 ads on Top-page. 150px

--- src/components/googleAdsense/index.js
+++ src/components/googleAdsense/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import Adsense from 'react-adsense'
 
 
-const AdSense1 = () => {
+const AdSense1 = ({ maxHeight = undefined }) => {
   return (
     <div>
       <Adsense.Google 
-        style={{display: "block"}}
+        style={{display: "block", maxHeight: maxHeight}}
         client="ca-pub-7416328580394075"
         slot="4572443902"
         format="auto"
